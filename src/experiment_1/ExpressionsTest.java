ddc411973ef3fa29a2ce4098ebc391b2b4d6e0ba
MrTuo <595983351@qq.com>
2016-11-08 10:24:03 +0800

    。。。 、

---
+++ src/experiment_1/ExpressionsTest.java
@@ -0,0 +1,24 @@
+package experiment_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExpressionsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSimplify() {
+		fail("Not yet implemented");
+	}
+
+}
